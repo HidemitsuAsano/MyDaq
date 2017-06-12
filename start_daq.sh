ef754ceeebda0f335d3b55e6e4206765b5d0b577
HidemitsuAsano <hasano@scphys.kyoto-u.ac.jp>
2017-06-12 21:11:00 +0900

    update

--- start_daq.sh
+++ start_daq.sh
@@ -2,8 +2,7 @@
 ./kill_daq.sh
 
 sleep 1
-xterm -T ReaderComp -bg lightblue -geom 100x20+1200+100 -e tail -F /tmp/daqmw/log.NIMEASIROCReaderComp &
-xterm -T MonitorComp -bg lightgreen -geom 100x20+1200+400 -e tail -F /tmp/daqmw/log.NIMEASIROCMonitorComp &
-xterm -T LoggerComp -bg lightyellow -geom 100x20+1200+700 -e tail -F /tmp/daqmw/log.NIMEASIROCLoggerComp &
-pwd
-run.py -cl config.xml
+xterm -T ReaderComp -bg lightblue -geom 100x20+1200+100 -e tailf  /tmp/daqmw/log.NIMEASIROCReaderComp &
+xterm -T MonitorComp -bg lightgreen -geom 100x20+1200+400 -e tailf /tmp/daqmw/log.NIMEASIROCMonitorComp &
+xterm -T LoggerComp -bg yellow -geom 100x20+1200+700 -e tailf /tmp/daqmw/log.NIMEASIROCLoggerComp &
+xterm -T Operator  -geom 100x20 -e 'run.py -cl config.xml' &
